a7af9e7425387c6fa6181658cc9399a7ec926d94
MichaelGee76 <gottungmichael@gmail.com>
2024-10-03 11:44:19 +0200

    calculator test written

--- test_calcuter.py
+++ test_calcuter.py
@@ -1,9 +1,24 @@
+import pytest
 from calcuter import square
 
 
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+
+def test_zero():
     assert square(0) == 0
+
+
+# mit der pytest method raises teile ich mit, dass ich einen typeerror erwarte.
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("5")
