28e59b8b48c29d3240c8f0305ae6c0db8d9530f6
MichaelGee76 <gottungmichael@gmail.com>
2024-10-12 23:06:01 +0200

    scourgify

--- scourgify.py
+++ scourgify.py
@@ -8,20 +8,23 @@ def main():
     input_data = sys.argv[1]
     output_data = sys.argv[2]
 
-    with open(input_data) as file:
-        reader = csv.reader(file)
-        next(reader)
+    try:
+        with open(input_data) as file:
+            reader = csv.reader(file)
+            next(reader)
 
-        with open(output_data, "a") as output_file:
-            writer = csv.writer(output_file)
-            writer.writerow(["First_Name", "Last_Name", "House"])
+            with open(output_data, "w") as output_file:
+                writer = csv.writer(output_file)
+                writer.writerow(["first", "last", "house"])
 
-            for line in reader:
-                name = line[0]
-                house = line[1]
-                last_name, first_name = name.split(", ")
+                for line in reader:
+                    name = line[0]
+                    house = line[1]
+                    last_name, first_name = name.split(", ")
 
-                writer.writerow([first_name, last_name, house])
+                    writer.writerow([first_name, last_name, house])
+    except FileNotFoundError:
+        sys.exit(f"Could not read {input_data}")
 
 
 if __name__ == "__main__":
